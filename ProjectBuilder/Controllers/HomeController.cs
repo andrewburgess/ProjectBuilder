c5da5e4deb87419b1721761f034f755cec5d41c9
Andrew Burgess <andrew@deceptacle.com>
2012-10-16 21:49:06 -0400

    Added some packages, set up base things

---
+++ ProjectBuilder/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting.Web.Mvc;
+
+namespace ProjectBuilder.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [GET("")]
+        [GET("index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
