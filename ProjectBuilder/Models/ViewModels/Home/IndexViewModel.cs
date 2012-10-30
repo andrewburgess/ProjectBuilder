11ad56addda984664f8068692af1d75a83506436
Andrew Burgess <andrew@deceptacle.com>
2012-10-30 12:12:23 -0400

    Added ViewModel for home page

---
+++ ProjectBuilder/Models/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace ProjectBuilder.Models.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public List<Project> Projects { get; set; }
+    }
+}
