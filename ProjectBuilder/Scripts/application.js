6e1d8aea1ad67793fd287507b5cfbd0c0a18a730
Andrew Burgess <andrew@deceptacle.com>
2012-12-22 20:48:21 -0500

    Modal dialog is hooked up

--- ProjectBuilder/Scripts/application.js
+++ ProjectBuilder/Scripts/application.js
@@ -1,1 +1,31 @@
-﻿
+﻿(function ($, ko) {
+
+    ko.bindingHandlers.modal = {
+        init: function (element, valueAccessor, bindingsAccessor, viewModel, bindingContext) {
+            var $element = $(element);
+            $element.addClass('hide modal');
+
+            var bindings = bindingsAccessor();
+            if (bindings.modalOptions) {
+                if (bindings.modalOptions.beforeClose) {
+                    $element.on('hide', function () {
+                        return bindings.modalOptions.beforeClose();
+                    });
+                }
+            }
+
+            return ko.bindingHandlers['with'].init.apply(this, arguments);
+        },
+        update: function (element, valueAccessor, bindingsAccessor, viewModel, bindingContext) {
+            var value = ko.utils.unwrapObservable(valueAccessor());
+            var returnValue = ko.bindingHandlers['with'].update.apply(this, arguments);
+
+            var $element = $(element);
+            if (value)
+                $element.modal('show');
+
+            return returnValue;
+        }
+    };
+
+} (jQuery, ko));
--- ProjectBuilder/Scripts/configuration/view-model.js
+++ ProjectBuilder/Scripts/configuration/view-model.js
@@ -1,18 +1,32 @@
-﻿function Node(data) {
-    this.Id = ko.observable(data.Id);
-    this.Name = ko.observable(data.Name);
-    this.Description = ko.observable(data.Description); 
-}
+﻿(function ($, ko) {
 
-function NodeListViewModel(data) {
-    var self = this;
+    function Node(data) {
+        this.Id = ko.observable(data.Id);
+        this.Name = ko.observable(data.Name);
+        this.Description = ko.observable(data.Description);
+    }
 
-    this.nodes = ko.observableArray($.map(data, function(item) {return new Node(item)}));
-}
+    function NodeListViewModel(data) {
+        var self = this;
 
-var PageViewModel = {
-    NodeListViewModel: new NodeListViewModel(nodes)
-};
+        this.nodes = ko.observableArray($.map(data, function (item) { return new Node(item) }));
 
-// Activates knockout.js
-ko.applyBindings(PageViewModel.NodeListViewModel);
+        this.modalNode = ko.observable();
+        this.editNode = function (node) {
+            this.modalNode(node);
+        };
+
+        this.addNode = function () {
+            var newNode = new Node({ Id: -1, Name: '', Description: '' });
+            this.modalNode(newNode);
+        };
+    }
+
+    var PageViewModel = {
+        NodeListViewModel: new NodeListViewModel(nodes)
+    };
+
+    // Activates knockout.js
+    ko.applyBindings(PageViewModel.NodeListViewModel);
+
+} (jQuery, ko));
